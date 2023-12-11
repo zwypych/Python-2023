d6dd3591fbde28873be0e4332e1c81f6faa52e95
zwypych <suzze.w@gmail.com>
2023-12-11 12:44:03 +0100

    zadanko

--- 05_Petle/for.py
+++ 05_Petle/for.py
@@ -21,3 +21,13 @@ for i in range(10):
         break
 else:
     print("Koniec")
+
+#zadanie 1
+for a in range(1, 100):
+    b = int(a // 10)
+    c = int(a % 10)
+    i = b + c
+    if (i % 7 == 0 and a % 2 == 0):
+        print(f" {a} liczba jest dobra")
+    # else:
+    #     print(f" {a} liczba jest zla")
