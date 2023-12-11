d31b9e56c152debb236edd1a52670c421bdea00b
zwypych <suzze.w@gmail.com>
2023-12-11 15:09:20 +0100

    zadanko

--- 06_Lancuchy_znakow/funkcje.py
+++ 06_Lancuchy_znakow/funkcje.py
@@ -29,3 +29,21 @@ napis = "ala ma kota"
 napis = napis.title().replace('Ala', 'Tomek')
 napis
 
+#zadanie - choinka
+liczba = int(input("Podaj liczbe"))
+for i in range(1, liczba):
+    print(f'{-i*" "}{i*"*"}{i*" "}')
+for j in range(1, 3):
+        print(f'{-j * " "}{j * "*"}')
+
+#rozwiazanie wspolne
+n = int(input("Podaj wysokosc:"))
+for i in range(n):
+    print(f'{" "*(n-i)}{(2*i+1)*"*"}')
+for i in range(n):
+    if i == 2:
+        break
+    print(f'{" " * (n - i)}{(2 * i + 1) * "*"}')
+
+
+
