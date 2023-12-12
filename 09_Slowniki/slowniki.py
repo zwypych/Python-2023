d217d178fc2530d421aea036d4d3d01995c3a2a4
zwypych <suzze.w@gmail.com>
2023-12-12 12:01:24 +0100

    zadanko

--- 09_Slowniki/slowniki.py
+++ 09_Slowniki/slowniki.py
@@ -145,3 +145,32 @@ else:
     napis += setki[n - n % 100] + " " + dziesiatki[n - n % 10] + " " + nazwy_jednosci[n % 10]
 
 print(napis)
+
+#drugie rozwiazanie
+
+nazwy_jednosci = {0: "", 1: "jeden", 2: "dwa", 3: "trzy", 4: "cztery", 5: "pięć", 6: "sześć", 7: "siedem",
+                  8: "osiem",
+                  9: "dziewięć"}
+nazwy_nast = {10: "dziesięć", 11: "jedenaście", 12: "dwanaście", 13: "trzynaście", 14: "czternaście",
+              15: "piętnaście", 16: "szesnaście", 17: "siedemnaście", 18: "osiemnaście",
+                  19: "dziewiętnaście"}
+nazwy_dziesiatek = {2: "dwadzieścia", 3: "trzydzieści", 4: "czterdzieści", 5: "pięćdziesiąt", 6: "sześćdziesiąt",
+                    7: "siedemdziesiąt", 8: "osiemdziesiąt", 9: "dziewięćdziesiąt"}
+
+nazwy_setek = {1: "sto", 2: "dwieście", 3: "trzysta", 4: "czterysta" , 5: "pięćset", 6: "sześćset", 7: "siedemset",
+               8: "osiemset", 9: "dziewięćset"}
+
+liczba = int(input("Wpisz cyfre"))
+
+g = int(liczba) // 100
+d = (liczba % 100 ) // 10
+j = liczba % 10
+lista_2 = []
+if g != 0:
+    lista_2.append(nazwy_setek[g])
+if d == 1:
+    lista_2.append(nazwy_nast[liczba % 100])
+else:
+    lista_2.append(nazwy_dziesiatek[d])
+    lista_2.append(nazwy_jednosci[j])
+print(" ".join(lista_2) )
