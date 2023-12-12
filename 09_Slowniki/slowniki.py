e024f2d33e00c138c638f8f920ada0a4ecaf66c4
zwypych <suzze.w@gmail.com>
2023-12-12 11:25:08 +0100

    zadanko

--- 09_Slowniki/slowniki.py
+++ 09_Slowniki/slowniki.py
@@ -3,13 +3,13 @@ s
 s = {1: "a", 2: 'b'}
 s
 
-s = {[1]: "a", 2: 'b'}  # Błąd
+s = {[1]: "a", 2: 'b'}  # Błąd - listy nie moga byc kluczami - lepiej zrobic krotke
 s = {tuple([1]): "a", 2: 'b'}
 s
 s = {"a": 1, 'b': 2}
 s
 s['a']
-s['c']  # Błąd
+s['c']  # Błąd - trzeba użyć istniejacego klucza
 s.get('a')
 s.get('c', 0)
 s['c'] = 3
@@ -22,14 +22,27 @@ s = {"a": 1, 'b': 2, 'c': 3}
 for k in s.keys():
     print(k)
 
+# ponizej tez iterujemy po kluczach
+for k in s:
+    print(k, s[k])
+
+for k in s:
+    print(k)
+
 for k in s.values():
     print(k)
 
 for k in s.items():
     print(k)
 
+s['a'] = 4
+
+for k in s.items():
+    print(k)
+
 for k, v in s.items():
-    print(k, v)
+    print(k)
+    print(v)
 
 nazwy_jednosci = {0: "", 1: "jeden", 2: "dwa", 3: "trzy", 4: "cztery", 5: "pięć", 6: "sześć", 7: "siedem", 8: "osiem",
                   9: "dziewięć"}
@@ -49,12 +62,86 @@ elif n == 5:
 else:
     print('tej liczby nie znam')
 
+# dodawanie slownikow
 s2 = {'d': 4}
-s|s2
+s | s2
 
-s|={'e':5}
+# dodaje element
+s |= {'e': 5}
 s
 
+# czy jest w slowniku
 'a' in s
 
 s = {1: "a", 2: 'b', "ala": [3, 4]}
+
+# zadanie
+'''Dla listy napisów pobranej w pętli z wejścia wypisać słownik ilości wystąpień napisów
+np. dla ['Ala', 'ma' 'kota', 'kota'] wypisać {'Ala': 1, 'ma': 1, ;'kota': 2}'''
+
+# slownik = {}
+# while True:
+#     i = input("Podaj slowo")
+#     if (i == ""):
+#         break
+#     for a in slownik:
+#         slownik[i] = a
+#
+# lista.sort(key=str)
+# print(lista)
+
+slow = {}
+
+while True:
+
+    wpis = input("Wpisz coś")
+    if wpis == "":
+        break
+    # slow.get (wpis)
+    print(slow)
+    n = slow.get(wpis, 0)
+    n += 1
+    slow[wpis] = n
+
+print(slow)
+
+lista = []
+
+from collections import Counter
+
+Counter(lista)
+Counter({'kota': 2, 'Ala': 1})
+
+from collections import Counter
+
+lista = []
+lista.append('ala')
+c = Counter(lista)
+print(c)
+
+c["ala2"]
+
+# zadanie 2
+'''Dla wczytanej liczby z wejścia z zakresu 1-999 wypisać jej postać słowną
+np. dla 73 wypisać siedemdziesiąt trzy'''
+
+nazwy_jednosci = {0: "", 1: "jeden", 2: "dwa", 3: "trzy", 4: "cztery", 5: "pięć", 6: "sześć", 7: "siedem", 8: "osiem",
+                  9: "dziewięć"}
+dziesiatki = {0: "", 10: "dziesiec", 20: "dwadziescia", 30: "trzydziesci", 40: "czterdziesci", 50: "piecdziesiat",
+              60: "szescdziesiat", 70: "siedemdziesiat", 80: "osiemdziesiat", 90: "dziewiecdziesiat"}
+
+nastki = {11: "jedenascie", 12: "dwanascie", 13: "trzynascie", 14: "czternascie", 15: "pietnascie",
+          16: "szesnascie", 17: "siedemnascie", 18: "osiemnascie", 19: "dziewietnascie"}
+
+setki = {0: "", 100: "sto", 200: "dwiescie", 300: "trzysta", 400: "czterysta", 500: "piecset", 600: "szescset",
+         700: "siedemset", 800: "osiemset", 900: "dziewiecset", 1000: "tysiac"}
+
+n = int(input("Wpisz liczbe:"))
+napis = setki[n-n % 100]
+n = n%100
+if n in range(11, 20):
+    napis += nastki[n]
+else:
+    napis += setki[n - n % 100] + " " + dziesiatki[n - n % 10] + " " + nazwy_jednosci[n % 10]
+
+print(napis)
