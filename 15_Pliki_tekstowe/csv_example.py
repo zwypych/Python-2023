b8dde87bfd312a9f87e58e7583cab7790c6dd5ad
zwypych <suzze.w@gmail.com>
2023-12-13 15:54:00 +0100

    zadanko

--- 15_Pliki_tekstowe/csv_example.py
+++ 15_Pliki_tekstowe/csv_example.py
@@ -1,6 +1,21 @@
-import csv
+# import csv
+#
+# with open('data\\foods.csv') as csvfile:
+#     reader = csv.DictReader(csvfile)
+#     for row in reader:
+#         print(row)
 
-with open('data\\foods.csv') as csvfile:
+
+import csv
+import sys
+
+data = []
+
+with open(sys.argv[1]) as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        print(row)
+        data.append(row)
+
+print(
+    sum([float(x['Budget']) for x in data if x['Director'] == sys.argv[2]])
+)
