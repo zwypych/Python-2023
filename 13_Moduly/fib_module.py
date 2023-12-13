91d54d547f75c9aeb5887181c0db92db3a7fa6ca
zwypych <suzze.w@gmail.com>
2023-12-13 11:27:27 +0100

    zadanko

--- 13_Moduly/fib_module.py
+++ 13_Moduly/fib_module.py
@@ -11,6 +11,6 @@ def fib(n):  # return Fibonacci series up to n
         a, b = b, a + b
     return result
 
-
+#main tylko pokaze w srodku skryptu ktory odpalam - rozpoznaje czy plik jest importowany czy nie
 if __name__ == '__main__':
     print(f'Testowo: {fib(15)}')
