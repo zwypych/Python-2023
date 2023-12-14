9e12b6188c1e1cc7ec132794d7b3c4b48718ddcc
zwypych <suzze.w@gmail.com>
2023-12-14 11:22:33 +0100

    zadanko

---
+++ 17_Wstep_do_obiektowosci/zadanie 1.py
@@ -0,0 +1,49 @@
+class Instrument:
+    def mysound(self):
+        return "!&$%$^%*"
+
+    def play(self):
+        print(f"{self.player}@{self.__class__.__name__}: {self.mysound()}")
+
+    def __init__(self, player):
+        self.player = player
+
+
+class Drums(Instrument):
+    def __init__(self, player):
+        super().__init__(player)
+
+    def mysound(self):
+        return "Booom Booooom"
+
+
+class Guitar(Instrument):
+    def __init__(self, player):
+        super().__init__(player)
+
+    def mysound(self):
+        return "Pilim pilim"
+
+
+class Saxophone(Instrument):
+    def __init__(self, player):
+        super().__init__(player)
+
+    def mysound(self):
+        return "Fiufiu"
+
+
+instrument = globals()[input("Podaj instrument:")]('Bob')
+instrument.play()
+
+if __name__ == '__main__':
+    orkiestra = []
+    for wpis in sys.argv[1:]:
+        muzyk, instrument = tuple(wpis.strip().split('@'))
+        orkiestra.append(
+            globals()[instrument](muzyk)
+        )
+    for instrument in orkiestra:
+        instrument.play()
+
+# w terminalu: python fabryka_cli.py Bill@Guitar John@Guitar Bob@Drums
