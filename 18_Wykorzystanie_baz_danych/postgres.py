ed48dc9dae4b0cfc5669cf18a6075f4e8e6fb2e0
zwypych <suzze.w@gmail.com>
2023-12-14 15:39:36 +0100

    poprawka

--- 18_Wykorzystanie_baz_danych/postgres.py
+++ 18_Wykorzystanie_baz_danych/postgres.py
@@ -2,10 +2,16 @@ import psycopg2
 
 connect_string = "dbname=my_database user=my_user password=secret host=127.0.0.1"
 
+stmt = """
+SELECT * from orders 
+join orders_item on orders_item.orders_id =orders.id 
+join fooditem on fooditem.id=orders_item.fooditem_id where customer_id=1;
+"""
+stmt2 = 'SELECT COUNT(*) from fooditem;'
 with psycopg2.connect(connect_string) as connection:
     try:
         cursor = connection.cursor()
-        cursor.execute('SELECT COUNT(*) from fooditem;')
+        cursor.execute(stmt)
         result = cursor.fetchall()
         for row in result:
             print (row)
